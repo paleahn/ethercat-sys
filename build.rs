44325e3970a74dc8c8f51c69a25233a73bb1df5a
paleahn <paleahn@gmail.com>
2026-01-07 13:43:53 +0900

    Fix the include_header

--- build.rs
+++ build.rs
@@ -4,25 +4,25 @@ fn main() {
     println!("cargo:rerun-if-env-changed=ETHERCAT_LIB_DIR");
     println!("cargo:rerun-if-env-changed=ETHERCAT_INCLUDE_DIR");
 
-    let include_header = match pkg_config::Config::new().probe("libethercat") {
+    let include_dir = match pkg_config::Config::new().probe("libethercat") {
         Ok(lib) => lib.include_paths.get(0).expect("No include path found").clone(),
         Err(_) => {
             let lib_dir = PathBuf::from(env::var("ETHERCAT_LIB_DIR").expect("Env var ETHERCAT_LIB_DIR not set"));
             if !lib_dir.join("libethercat.so").exists() {
                 panic!("Couldn't find libethercat.so")
-            }
-
-            let include_dir = PathBuf::from(env::var("ETHERCAT_INCLUDE_DIR").expect("Env var ETHERCAT_INCLUDE_DIR not set"));
-            if !include_dir.join("ecrt.h").exists() {
-                panic!("Couldn't find ecrt.h")
-            }
+            } 
 
             println!("cargo:rustc-link-search=native={}", lib_dir.display());
             println!("cargo:rustc-link-lib=dylib=ethercat");
 
-            include_dir
+            PathBuf::from(env::var("ETHERCAT_INCLUDE_DIR").expect("Env var ETHERCAT_INCLUDE_DIR not set"))
         }
     };
+
+    let include_header = include_dir.join("ecrt.h");
+    if !include_header.exists() {
+        panic!("Couldn't find ecrt.h")
+    }
 
     bindgen::Builder::default()
         .header(include_header.to_string_lossy()) 
